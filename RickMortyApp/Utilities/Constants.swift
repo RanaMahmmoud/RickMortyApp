4d4bab19a78520cba25c51fc2ca2247656f0e472
Rana mahmoud <rana.mahmmoud@gmail.com>
2025-02-19 09:24:04 +0200

    Update Constants.swift

--- RickMortyApp/Utilities/Constants.swift
+++ RickMortyApp/Utilities/Constants.swift
@@ -10,18 +10,6 @@ import Foundation
 // MARK: -API Pagination Details
 let pageSize = 20
 
-// MARK: -API URL
-
-let baseURL = "https://rickandmortyapi.com/api/"
-let getCharactersURL = "\(baseURL)character/?count=\(pageSize)&page="
-let getCharactersDetailsURL = "\(baseURL)character/"
-let getFilteredCharacters = "\(getCharactersDetailsURL)?status="
-
 // MARK: -Page's Names
 let Characters = "Characters"
 
-enum URLConstant {
-   static let getCharactersURL = "\(baseURL)character/?count=\(pageSize)&page="
-   static let getCharactersDetailsURL = "\(baseURL)character/?count=\(pageSize)&page="
-   static let getFilteredCharacters = "\(baseURL)character/?count=\(pageSize)&page="
-}
